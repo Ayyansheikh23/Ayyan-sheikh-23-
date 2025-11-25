210af95c09b90746d3e8fca6824721f8afd5a13b
YourName <laptopworker23@gmail.com>
2025-11-24 17:49:32 -0800

    class work

---
+++ javascript/js class4/app.js
@@ -0,0 +1,27 @@
+
+// var totalCost = 1 + (3 * 4);
+// var totalCost = (1 +3) * 4;
+
+
+// document.write('totalCost1 :' )
+
+
+
+
+// var name = prompt (' Enter Your Name');
+// var phone = prompt( 'enter your number')
+
+
+// console.log(name);
+
+// var num1 = +prompt('enter your first number')
+// var num2 = +prompt('enter your second number') 
+
+// document.write(Number(num1) + Number(num2));
+
+var x = prompt("Where does the Pope live?");
+  if (x === "Vatican") {
+    alert("Correct!");
+  } else{
+    alert('wrong answer')
+  }
