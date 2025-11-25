2d4397a659d05bccad3985b57dd73c723c503c36
YourName <laptopworker23@gmail.com>
2025-11-25 23:34:12 +0500

    Home Work

---
+++ javascript/js HomeWork2/app.js
@@ -0,0 +1,26 @@
+var age = 18;  
+alert("My age is: " + age);
+
+
+
+var visits = localStorage.getItem("visits");
+
+
+if (visits === null) {
+    visits = 1;
+} else {
+    visits = Number(visits) + 1;
+}
+
+
+localStorage.setItem("visits", visits);
+
+
+alert("You have visited this site " + visits + " times");
+
+ 
+
+ var birthYear = 2005;
+
+    document.write("My birth year is " + birthYear + "<br>");
+    document.write("Data type of my declared variable is number");
