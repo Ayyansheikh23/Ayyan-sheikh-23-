0bfbcdcf634ff1131bbb50f70251197d1483a29d
YourName <laptopworker23@gmail.com>
2025-11-26 17:57:49 +0500

    class work

---
+++ javascript/js class5/app.js
@@ -0,0 +1,28 @@
+// if else 
+
+
+
+ // data type 
+
+//  primitive data types
+// 1. 'string'
+// 2. 'number'
+// 3. 'Undefiend'
+// 4. null 
+
+
+
+
+
+// non primitive data types
+
+
+var myName = undefined
+var myName = null
+let name1 = undefined
+name1 = ''
+var name3 = 0
+
+// war = 12;
+
+    
