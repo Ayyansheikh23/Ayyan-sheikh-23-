91ac95d0ce57edc822c8fc2db2d088787d972528
YourName <laptopworker23@gmail.com>
2025-12-05 17:45:55 +0500

    class work

---
+++ javascript/js class6/app.js
@@ -0,0 +1,38 @@
+// var player = prompt('from which city you are' )
+// var score = 0;
+// var userIQ ='not available';
+
+
+// var city = "karachi";
+//   if(player === city){
+//     score++;
+//     userIQ = "genius";
+//     document.write(userIQ+ '<br>');
+//      document.write(score);
+//     alert('you can come')
+//   }
+//   else{
+//     alert('you are not allow')
+//   }
+
+
+// (x + y === a - b)
+
+// var x = 10;
+// var y = 10;
+// var a = 40;
+// var b = 40;
+
+// if(x + y !== a - b){
+//     alert('yes it is false');
+// }
+
+var age = 30
+var correction = 'Age is correct'
+var notcorrect = 'Your age is wrong';
+
+if (age < 20 ){
+    alert(correction)
+}else{
+     alert(notcorrect);
+}
