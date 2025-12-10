6f8818060f36947e9d9abdbe0a6b12bfc75dd812
YourName <laptopworker23@gmail.com>
2025-12-10 17:52:09 +0500

    class work

---
+++ javascript/js class9/app.js
@@ -0,0 +1,39 @@
+// var fruits = ['banana', 'mango', 'apple'];
+
+
+// console.log( 'before pop'  ,fruits)
+
+
+// fruits.pop();
+
+
+// console.log('after pop',fruits);
+
+// fruits.push('pear', 'kiwi','orange')
+
+// console.log('after push 3 friuts', fruits);
+
+
+// var fruits = ['banana', 'mango', 'apple'];
+
+
+// console.log('123', fruits);
+
+// fruits.shift();
+
+
+// console.log('after shift', fruits);
+
+
+// fruits.unshift('strawberry', 'avocadro');
+
+
+// var fruits = ['banana', 'mango', 'apple'];
+
+// fruits.splice(1,1,'lychee', 'pineapple')
+
+// console.log('after',fruits);
+
+
+var fruits = ['banana', 'mango', 'apple'];
+
