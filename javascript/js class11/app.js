1dc75353d1d0b07151b37e11da46cf7fc2972f3c
YourName <laptopworker23@gmail.com>
2025-12-15 17:43:35 +0500

    class work

---
+++ javascript/js class11/app.js
@@ -0,0 +1,37 @@
+function  calculator(n1, n2, operate ){
+    var total;
+    if(operate === '+'){
+        total = n1 + n2; 
+    }else if (operate === '-'){
+        total= n1 - n2;
+    }else if (operate === '*'){
+        total= n1 * n2;
+    }else if (operate === '/'){
+        total= n1 / n2;
+    }else{
+        alert('plz enter right number or operator ');
+    };
+
+    
+    return total   
+
+}
+
+var num1 = +prompt('enter num1');
+var num2 = +prompt('enter num2');
+var operator = prompt('enter operator'); 
+
+calculator(num1, num2, operator);
+
+var output = calculator(num1, num2, operator );
+
+
+// var heading = document.getElementById('hello');
+// heading.innerHTML = output;
+
+
+var heading = document.createElement('h1');
+
+heading.innerHTML = output;
+
+document.body.appendChild(heading);
