c29442f75b1073798732b488ebaadb7ec756f31c
YourName <laptopworker23@gmail.com>
2026-01-19 00:07:32 +0500

    Home Work

---
+++ javascript/js class14/app.js
@@ -0,0 +1,37 @@
+// var dob = new Date(prompt(`write age in this (12, august 1999)format`));
+// var dobTime = dob.getTime();
+// console.log('dobTime', dobTime)
+
+
+// var date = new Date();
+// var dTime = date.getTime()
+// console.log('dTime',dTime)
+
+// var calAge = dTime - dobTime;
+// var final = Math.floor(calAge / (1000 * 60 * 60 * 24 * 365));
+// console.log('final' , final)
+
+// switch
+
+// var userAge = 30;
+
+// switch (userAge) {
+//     case 20:
+//         document.write('age is correct')
+//         break;
+//     case 10:
+//         document.write('age is 10')
+
+//     default:
+//         document.write(' plz ennter correct age')
+//         break;
+
+// }
+
+
+// var i = 0;
+// do{
+//     alert(i);
+//     i++;
+// } while(i <= 5);
+
